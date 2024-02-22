b6c89af7ea676149fe2f5b116f90d04e099e07be
Damir Jelić <poljar@termina.org.uk>
2024-02-22 10:47:03 +0100

    contrib: Respond with a fake well-known in the well-known-block

--- contrib/mitmproxy/well-known-block.py
+++ contrib/mitmproxy/well-known-block.py
@@ -22,11 +22,35 @@ from mitmproxy import http
 
 def request(flow):
     if flow.request.path == "/.well-known/matrix/client":
-        flow.response = http.HTTPResponse.make(
-            404,  # (optional) status code
-            b"Not found",  # (optional) content
-            {"Content-Type": "text/html"}  # (optional) headers
+        headers = http.Headers()
+        headers.add("Content-Type", "application/json")
+
+        flow.response = http.Response.make(
+            status_code=200,  # (optional) status code
+            content=json.dumps(
+                {
+                    "m.homeserver": {"base_url": "https://localhost:8010/"},
+                    "org.matrix.msc2965.authentication": {
+                        "issuer": "https://localhost:8010",
+                        "account": "https://localhost:8010/account",
+                    },
+                    "org.matrix.msc3575.proxy": {"url": "https://localhost:8010"},
+                }
+            ),
+            headers=headers,
         )
+
+
+# def request(flow):
+#     if flow.request.path == "/.well-known/matrix/client":
+#         headers = http.Headers()
+#         headers.add("Content-Type", "application/json")
+#
+#         flow.response = http.HTTPResponse.make(
+#             status_code=404,  # (optional) status code
+#             content=b"Not found",  # (optional) content
+#             headers=headers,
+#         )
 
 
 def response(flow: http.HTTPFlow):
